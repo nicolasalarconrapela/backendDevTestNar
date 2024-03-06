51cffb57882ac1558c732c0426b5b5628e6eeb12
nicolasalarconrapela <nicolasalarconrapela@gmail.com>
2024-03-06 14:36:21 +0100

    Upd. V.0.0.1

---
+++ src/main/java/es/nunegal/api/nar/backenddevtestnar/BackenddevtestnarApplication.java
@@ -0,0 +1,13 @@
+package es.nunegal.api.nar.backenddevtestnar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackenddevtestnarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackenddevtestnarApplication.class, args);
+	}
+
+}
